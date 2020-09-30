4b4cd4672d2ff2e7604ca59bdc867a4b778004f3
marsha <marshaannon@gmail.com>
2020-09-30 10:11:06 -0400

    complete exercise

--- CountDuplicates.js
+++ CountDuplicates.js
@@ -1,8 +1,17 @@
 class CountDuplicates {
-
-    countDups(input){
-        return null;
+    countDups(input) {
+      let obj = {};
+      let count = 0;
+      for (let element of input) {
+        obj[element] = obj[element] + 1 || 1;
+      }
+      for (let value in obj) {
+        if (obj[value] > 1) {
+          count += 1;
+        }
+      }
+      return count;
     }
-}
+}    
 
 module.exports = CountDuplicates;
